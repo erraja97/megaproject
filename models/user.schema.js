44a193c88d072c6e4c100b98b007f8dfd9da4b36
Raja Gupta <erraja97@gmail.com>
2022-12-20 20:44:48 +0530

    Added more fields

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,10 +1,36 @@
 const mongoose = require("mongoose");
+import AuthRoles from "../utils/authRoles";
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is Required"],
-    maxLength: [30, "Name must be less than 30 characters"],
-    trim: true,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required"],
+      maxLength: [30, "Name must be less than 30 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+      minLength: [8, "Password must be atleast 8 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
